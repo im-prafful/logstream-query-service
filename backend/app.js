57ac68caacbb2f0c188f5d74e661a271866e6145
Hunter-Prash <rajaji.prashant@gmail.com>
2025-12-05 00:18:30 +0530

    cors policy added

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import authRoutes from './routes/authRoute.js';
+import cors from 'cors'
 const app = express();
 
+app.use(cors())
 app.use(express.json()); 
 
 //TEST ROUTE 1
