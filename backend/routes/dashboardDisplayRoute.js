a5fac354c66a0deb29958eb364c0e4f154173d2a
Hunter-Prash <rajaji.prashant@gmail.com>
2025-12-18 18:19:31 +0530

    added jwt middleware

--- backend/routes/dashboardDisplayRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { displayDashboard } from "../controllers/dashboardDisplayController.js";
-
-const router = express.Router();
-
-router.get("/logs", displayDashboard);
-
-export default router;
