fa58dc2f68677fbf73aea0f48c896a0f5e815b50
Hunter-Prash <rajaji.prashant@gmail.com>
2026-02-27 19:35:51 +0530

    feat: add controller to retrieve logs by cluster ID with Redis caching.

--- backend/controllers/exploreClustersCont.js
+++ backend/controllers/exploreClustersCont.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import query from '../dbConnector.js'
+import redisclient from '../caching/redisClient.js';
 
 export const getLogsByClusterId = async (req, res) => {
     try {
@@ -11,6 +12,15 @@ export const getLogsByClusterId = async (req, res) => {
                 message: 'clusterId is required'
             });
         }
+
+        const cachedData = await redisclient.get(`logsByClusterId:${clusterId}`);
+        if (cachedData) {
+            return res.status(200).json({
+                message: 'success',
+                logs: JSON.parse(cachedData)
+            });
+        }
+
 
         const result = await query(`
             WITH t AS (
@@ -26,6 +36,8 @@ export const getLogsByClusterId = async (req, res) => {
             ORDER BY cluster_id
             LIMIT 10;
     `);
+
+        await redisclient.setEx(`logsByClusterId:${clusterId}`, 120, JSON.stringify(result.rows));
 
         return res.status(200).json({
             message: 'Success',
