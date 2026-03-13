58b6f4cb7ee4ed64f5bfa80ba74c61fe9d08db95
Hunter-Prash <rajaji.prashant@gmail.com>
2026-03-13 19:45:41 +0530

    changes

--- backend/controllers/incidentsController.js
+++ backend/controllers/incidentsController.js
@@ -1,10 +1,33 @@
+import query from "../dbConnector.js";
+
+
 export const viewIncidents = (req, res) => {
     return res.status(200).json({ message: 'incidents fetched successfully' });
 };
 
-export const createIncident = (req, res) => {
-    return res.status(201).json({ message: 'incident created successfully' });
+
+export const createIncident = async (req, res) => {
+    try {
+        const { cluster_id, assigned_role, assigned_to } = req.body;
+
+        await query(`
+            INSERT INTO incidents (cluster_id, assigned_role, assigned_to)
+            VALUES (${cluster_id}, '${assigned_role}', '${assigned_to}')
+        `);
+
+        return res.status(201).json({
+            message: "incident created successfully"
+        });
+
+    } catch (err) {
+        console.error("Error creating incident:", err);
+
+        return res.status(500).json({
+            message: "failed to create incident"
+        });
+    }
 };
+
 
 export const deleteIncident = (req, res) => {
     return res.status(200).json({ message: 'incident deleted successfully' });
